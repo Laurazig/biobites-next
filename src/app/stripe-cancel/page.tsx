fed1d2d1169995817cb159a62eed1ee9be5212d1
Laura Hesse <lhzig@protonmail.com>
2023-08-22 21:38:55 +0200

    stripe pages

--- src/app/stripe-cancel/page.tsx
+++ src/app/stripe-cancel/page.tsx
@@ -2,9 +2,11 @@
 export default function stripeSuccess() {
     return (
         <div className=''>
-            <div className='md:m-10'>
+            <div className='md:m-10 h-1/4 items-center text-center rounded-sm p-1 m-3 '>
                 <h1>Payment Cancelled</h1>
-                <p></p>
+
+                <h3>Please check your bank account and then  try again.</h3>
+                <h3>Contact our customer services team if you have any questions</h3>
             </div>
         </div>
     )
--- src/app/stripe-success/page.tsx
+++ src/app/stripe-success/page.tsx
@@ -1,10 +1,15 @@
 
 export default function stripeSuccess() {
+//     const navigate = useNavigate();
+//   useEffect(()=>{
+//      navigate("/")
+//   },5000)
     return (
         <div className=''>
-            <div className='md:m-10'>
+            <div className='md:m-10 h-1/4 items-center text-center rounded-sm p-1 m-3 '>
                 <h1>Successful Payment</h1>
-                <p></p>
+                <h2>Payment Success! </h2>
+                <h3>We are now processing your order</h3>
             </div>
         </div>
     )
