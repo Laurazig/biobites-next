06e5c3d92915f2a480f9172c52c7b3e7703035b0
Laura Hesse <lhzig@protonmail.com>
2023-09-04 22:28:13 +0200

    vercel errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,16 +13,17 @@ import logoVegan from '../../public/assets/logos/petaLogo.jpg'
 import logoBio from '../../public/assets/logos/bioLogo.jpg'
 import logoProtein from '../../public/assets/logos/highProteinLogo.jpg'
 
-async function getPosts() {
-  const res = await fetch(`${process.env.BASE_URL}/api/getPosts`)
-  if(!res.ok){
-    console.log(res)
-  }
-  return res.json()
-}
+// async function getPosts() {
+//   //undefined/api/getPosts
+//   const res = await fetch(`${process.env.BASE_URL}/api/getPosts`)
+//   if(!res.ok){
+//     console.log(res)
+//   }
+//   return res.json()
+// }
 export default async function Home() {
-  const data = await getPosts()
-  console.log(data)
+  // const data = await getPosts()
+  // console.log(data)
 
 
 
